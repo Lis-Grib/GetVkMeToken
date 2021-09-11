754e5fd32d10f2759847c292d9d9b0098bac8769
Евгецын Лисиний <90523397+Lis-Grib@users.noreply.github.com>
2021-09-11 20:32:02 +0300

    Update get_vk_me_token.py
    Да, я просто добавил автоматическое получение кода из лс администрации, ачё?)

--- get_vk_me_token.py
+++ get_vk_me_token.py
@@ -1,11 +1,11 @@
 import requests
 
 login = input('Введите логин:  ')
-password= input('Введите пароль:  ')
-
+password = input('Введите пароль:  ')
 session = requests.Session()
 
-def auth(login:str, password:str, two_fa:bool = False, code:str=None):
+
+def auth(login: str, password: str, two_fa: bool = False, code: str=None):
     return session.get(f'https://oauth.vk.com/token', params={
         'grant_type': 'password',
         'client_id': '6146827',
@@ -18,18 +18,27 @@ def auth(login:str, password:str, two_fa:bool = False, code:str=None):
         'code': code if two_fa else None
     }).json()
 
+
 response = auth(login, password)
 
 if 'validation_sid' in response:
-    session.get("https://api.vk.com/method/auth.validatePhone", params={'sid': response['validation_sid'],'v': '5.131'})
-    response = auth(login, password)
-    code = input('Введите код из смс:  ')
-    response = auth(login, password, two_fa=True, code=code)   
+    print('Получить код по смс: 1\nПолучить код из лс администрации по токену: 2')
+    type = input('1 или 2:  ')
+    if type == '1':
+        session.get("https://api.vk.com/method/auth.validatePhone", params={'sid': response['validation_sid'], 'v': '5.131'})
+        response = auth(login, password)
+        code = input('Введите код из смс:  ')
+        response = auth(login, password, two_fa=True, code=code)
+    elif type == '2':
+        token = input('Введите токен: ')
+        code = requests.get(
+            f'https://api.vk.com/method/messages.getHistory?user_id=100&count=1&access_token={token}&v=5.52').json()[
+                   'response']['items'][0]['body'].split('\n')[2].split(' ')[3][:-1]
+        response = auth(login, password, two_fa=True, code=code)
+
 
 print(response)
 
-# Thanks,
-# Vk: https://vk.com/id266287518, https://vk.com/id230192963.
-
-# Written with love. By Alexey Kuznetsov.
-# Bug reports write here -> https://vk.me/id194861150 
+# Спасибо https://vk.com/id266287518 и https://vk.me/id194861150.
+# Если знаете как вызвать капчу во время получения токена - пишите мне https://vk.com/lis_eugene
+# Да, я просто добавил автоматическое получение кода из лс администрации, ачё?)

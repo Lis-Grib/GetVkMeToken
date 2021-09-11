3dec6311f7a05effd51bbaf7c94dd86aaea79382
Евгецын Лисиний <90523397+Lis-Grib@users.noreply.github.com>
2021-09-11 21:33:05 +0300

    Update and rename get_vk_me_token.py to get_vk_token.py

--- get_vk_token.py
+++ get_vk_token.py
@@ -1,15 +1,21 @@
 import requests
 
+client = input('Получаем токен\n1)VK Official\n2)VK Me\n1 или 2:  ')
+client = 'me' if client == 2 else 'off'
 login = input('Введите логин:  ')
 password = input('Введите пароль:  ')
+
+
 session = requests.Session()
 
 
-def auth(login: str, password: str, two_fa: bool = False, code: str=None):
+def auth(login: str, password: str, client: str, two_fa: bool = False, code: str=None):
+    clients_ids = {'me': '6146827', 'off': '2274003'}
+    clients_secrets = {'me': 'qVxWRF1CwHERuIrKBnqe', 'off': 'hHbZxrka2uZ6jB1inYsH'}
     return session.get(f'https://oauth.vk.com/token', params={
         'grant_type': 'password',
-        'client_id': '6146827',
-        'client_secret': 'qVxWRF1CwHERuIrKBnqe',
+        'client_id': clients_ids[client],
+        'client_secret': clients_secrets[client],
         'username': login,
         'password': password,
         'v': '5.131',
@@ -19,26 +25,24 @@ def auth(login: str, password: str, two_fa: bool = False, code: str=None):
     }).json()
 
 
-response = auth(login, password)
-
+response = auth(login, password, client)
 if 'validation_sid' in response:
     print('Получить код по смс: 1\nПолучить код из лс администрации по токену: 2')
     type = input('1 или 2:  ')
     if type == '1':
-        session.get("https://api.vk.com/method/auth.validatePhone", params={'sid': response['validation_sid'], 'v': '5.131'})
-        response = auth(login, password)
+        session.get("https://api.vk.com/method/auth.validatePhone",
+                    params={'sid': response['validation_sid'], 'v': '5.131'})
+        response = auth(login, password, client)
         code = input('Введите код из смс:  ')
-        response = auth(login, password, two_fa=True, code=code)
+        response = auth(login, password, client, two_fa=True, code=code)
     elif type == '2':
         token = input('Введите токен: ')
         code = requests.get(
             f'https://api.vk.com/method/messages.getHistory?user_id=100&count=1&access_token={token}&v=5.52').json()[
                    'response']['items'][0]['body'].split('\n')[2].split(' ')[3][:-1]
-        response = auth(login, password, two_fa=True, code=code)
+        response = auth(login, password, client, two_fa=True, code=code)
+print(f'Токен: {response["access_token"]} \nUser ID: {response["user_id"]}')
 
-
-print(response)
-
-# Спасибо https://vk.com/id266287518 и https://vk.me/id194861150.
+# Спасибо https://vk.com/id266287518, https://vk.me/id194861150 и https://github.com/vodka2.
 # Если знаете как вызвать капчу во время получения токена - пишите мне https://vk.com/lis_eugene
-# Да, я просто добавил автоматическое получение кода из лс администрации, ачё?)
+# Да, я просто добавил автоматическое получение кода из лс администрации и получение токена офф версии, ачё?)
